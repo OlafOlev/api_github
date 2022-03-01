8f4286c22cfb76fc56701c20cbaab6d026985d0b
Olaf Olev Alakivi <olaf-olev.alakivi@khk.ee>
2022-03-01 13:53:36 +0200

    ylesanne1

---
+++ ylesanne1.py
@@ -0,0 +1,10 @@
+import pygal
+from pygal.style import  Style
+custom_style = Style(
+    colors=("#E80080", "#404040", "#9BC850"))
+b_chart = pygal.Bar(style=custom_style)
+b_chart.title = "Destiny Kill/Death Ration"
+b_chart.add("Dijiphos", [0.94])
+b_chart.add("Punisherdonk", [1.05])
+b_chart.add("Musclemuffins20", [1.10])
+b_chart.render_in_browser()
